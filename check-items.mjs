0801fbfc1bce36a20d04bdfd05046b037d033e06
arifmmmaulana <arifmmmaulana@gmail.com>
2026-08-05 21:15:08 +0700

    chore: remove temporary debug scripts

--- check-items.mjs
+++
@@ -1,7 +0,0 @@
-import { supabase } from './src/lib/supabase.js';
-
-async function checkItems() {
-  const { data } = await supabase.from('items').select('*');
-  console.log(data);
-}
-checkItems();
